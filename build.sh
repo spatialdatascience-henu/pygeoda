0dfe28e68199b5c7b0e358213a12dbbd90dfbdff
Xun Li <xunli@asu.edu>
2019-12-05 11:40:01 -0700

    update build for windows

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 python setup.py bdist_wheel
 #MACOSX_DEPLOYMENT_TARGET=10.9 python3 setup.py bdist_wheel
 #python setup.py sdist
+#c:\python27_amd64\python.exe setup.py bdist_wininst
+#c:\python27_amd64\python.exe setup.py bdist_wheel
+# rename files
 #twine upload dist/*
 
